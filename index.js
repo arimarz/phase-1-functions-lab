68f209c3e3f10f91cdbbc0310b46bd3152fc91b4
Ari <aritheriver@gmail.com>
2023-01-11 17:09:10 -0800

    lab finished

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+};
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264; 
+};
+console.log(distanceFromHqInBlocks());
+console.log(distanceFromHqInFeet());
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264);
+}
+function calculatesFarePrice(start, destination) {
+    const travelledInFeet = distanceTravelledInFeet(start, destination)
+    if (  travelledInFeet <= 400) {
+        return 0;
+    } else if (travelledInFeet > 2500) {
+        return 'cannot travel that far';
+    } else if (travelledInFeet > 400 && travelledInFeet <= 2000) {
+        return (travelledInFeet - 400) * 0.02;
+    } else if (travelledInFeet <= 2500 && travelledInFeet > 2000) {
+        return 25;
+    }
+};
+
